3c8424954f1ac41a29d58e222e4f227ad9991a56
peymanras <peyman.rasouli1@gmail.com>
2020-07-08 12:06:18 +0200

    initial commit

--- data_visualization.py
+++ data_visualization.py
@@ -2,6 +2,7 @@ from EXPLAN.utils import *
 from sklearn.ensemble import RandomForestClassifier
 from treeinterpreter import treeinterpreter as ti
 from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings("ignore")
@@ -18,12 +19,16 @@ datsets_list = {
     'adult': ('adult.csv', prepare_adult_dataset)
 }
 
-# Reading a data set
-dataset_name, prepare_dataset_fn = datsets_list['german']
-dataset = prepare_dataset_fn(dataset_name, path_data)
+# Selecting the data and the dimensionality reduction method
+data = 'german' # 'german' | 'compas' | 'adult'
+method = TSNE    # TSNE | PCA
 
+# Reading a data set
+dataset_name, prepare_dataset_fn = datsets_list[data]
+dataset = prepare_dataset_fn(dataset_name, path_data)
 X,y = dataset['X'], dataset['y']
 
+# Extracting feature contributions using TreeInterpreter
 blackbox = RandomForestClassifier(n_estimators=200)
 blackbox.fit(X,y)
 prediction, bias, contributions = ti.predict(blackbox, X)
@@ -31,22 +36,21 @@ contributions_ = np.zeros(np.shape(X))
 for i in range(len(contributions_)):
     contributions_[i, :] = contributions[i, :, np.argmax(prediction[i])]
 
-tsne_X =  TSNE(n_components=2)
-X_2D = tsne_X.fit_transform(X)
+# Dimensionality reduction
+X2D = method(n_components=2).fit_transform(X)
+C2D = method(n_components=2).fit_transform(contributions_)
 
-tsne_C =  TSNE(n_components=2)
-C_2D = tsne_C.fit_transform(contributions_)
-
+# Plotting the distribution of the data in both spaces
 color = ['tab:blue' if l==0 else 'tab:pink' for l in y]
 
 plt.subplot(121)
-plt.scatter(X_2D[:, 0], X_2D[:, 1], s=6, c=color)
+plt.scatter(X2D[:, 0], X2D[:, 1], s=6, c=color)
 plt.title("Feature Values")
 plt.xticks([])
 plt.yticks([])
 
 plt.subplot(122)
-plt.scatter(C_2D[:, 0], C_2D[:, 1], s=6, c=color)
+plt.scatter(C2D[:, 0], C2D[:, 1], s=6, c=color)
 plt.title("Feature Contributions")
 plt.xticks([])
 plt.yticks([])
